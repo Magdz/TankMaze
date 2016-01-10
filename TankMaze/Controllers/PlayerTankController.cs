82439a62ff7dae4adc7def1234d8c43e8cc73afb
Ahmed Magdy Ibrahim <magdz_008@yahoo.com>
2016-01-10 13:10:15 +0200

    Fix run problem

--- TankMaze/Controllers/PlayerTankController.cs
+++ TankMaze/Controllers/PlayerTankController.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Controls;
-using System.Windows.Media.Imaging;
+﻿using System.Windows.Controls;
 using TankMaze.Models;
 
 namespace TankMaze.Controllers
@@ -18,40 +12,6 @@ namespace TankMaze.Controllers
         {
             this.Ground = Ground;
             this.theTank = theTank;
-        }
-
-        public void Move(Windows.System.VirtualKey Key)
-        {
-            if (Key == SysteVirtualKey.Up)
-            {
-                if (Ground.GetRow(theTank) == 0) return;
-                RedTank.SetValue(Grid.RowProperty, Grid.GetRow(RedTank) - 1);
-                RedTank.SetValue(Grid.ColumnSpanProperty, 1);
-                RedTank.SetValue(Grid.RowSpanProperty, 2);
-                RedTank.Source = new BitmapImage(new Uri("ms-appx:///Assets/RedTankUp.png"));
-            }
-            else if (Key == Windows.System.VirtualKey.Down)
-            {
-                RedTank.SetValue(Grid.RowProperty, Grid.GetRow(RedTank) + 1);
-                RedTank.SetValue(Grid.ColumnSpanProperty, 1);
-                RedTank.SetValue(Grid.RowSpanProperty, 2);
-                RedTank.Source = new BitmapImage(new Uri("ms-appx:///Assets/RedTankDown.png"));
-            }
-            else if (Key == Windows.System.VirtualKey.Right)
-            {
-                RedTank.SetValue(Grid.ColumnProperty, Grid.GetColumn(RedTank) + 1);
-                RedTank.SetValue(Grid.ColumnSpanProperty, 2);
-                RedTank.SetValue(Grid.RowSpanProperty, 1);
-                RedTank.Source = new BitmapImage(new Uri("ms-appx:///Assets/RedTankRight.png"));
-            }
-            else if (Key == Windows.System.VirtualKey.Left)
-            {
-                if (Grid.GetColumn(RedTank) == 0) return;
-                RedTank.SetValue(Grid.ColumnProperty, Grid.GetColumn(RedTank) - 1);
-                RedTank.SetValue(Grid.ColumnSpanProperty, 2);
-                RedTank.SetValue(Grid.RowSpanProperty, 1);
-                RedTank.Source = new BitmapImage(new Uri("ms-appx:///Assets/RedTankLeft.png"));
-            }
         }
     }
 }
---
+++ TankMaze/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using TankMaze.Views;
+
+namespace TankMaze
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            this._NavigationFrame.Navigate(new PlayGround());
+        }
+    }
+}
