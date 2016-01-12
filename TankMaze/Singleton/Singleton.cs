655d1d2c442b546a2dd12f5395b50eca4fa00502
Nader Alsharkawy <subnader@gmail.com>
2016-01-12 03:24:15 +0200

    Create Singleton class
    Singleton class creation with double checking for multithreading
    handling

---
+++ TankMaze/Singleton/Singleton.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TankMaze.Singleton
+{
+    class Singleton
+    {
+        Singleton()
+        {
+        }
+
+        //Shared object for thread safety
+        private static readonly object threadLock = new object();
+
+        //Sole instance initialization
+        private static Singleton singletonInstance = null;
+
+        public static Singleton Instance
+        {
+            get
+            {
+                //Double checking
+                if (singletonInstance == null)
+                {
+                    //Shared object lock on instantiation
+                    lock (threadLock)
+                    {
+                        if (singletonInstance == null)
+                        {
+                            //Sole instance instantiation 
+                            singletonInstance = new Singleton();
+                        }
+                    }
+                }
+
+                //Return sole instance
+                return singletonInstance;
+            }
+        }
+    }
+}
