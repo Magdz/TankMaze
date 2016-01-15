f81a9c3dd75d032f5d5d29db3f77f6c0058d54a0
Basma Adrii <basma.adrii@gmail.com>
2016-01-15 16:28:13 +0200

    Edit Factory and Object Pool

---
+++ TankMaze/Factory/MazeFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TankMaze.Models;
+using TankMaze.Object_Pool;
+
+namespace TankMaze.Factory
+{
+    static class MazeFactory
+    {
+        public static void createObject (ObjectPool.Type type, int Row, int Column, MazeComponent.Direction direction)
+        {
+            if (type == ObjectPool.Type.Ammo) ObjectPool.addObject(type, new Ammo(Row, Column, direction));
+            else if (type == ObjectPool.Type.BagsWall) ObjectPool.addObject(type, new BagsWall(Row, Column, direction));
+            else if (type == ObjectPool.Type.Bomb) ObjectPool.addObject(type, new Bomb(Row, Column, direction));
+            else if (type == ObjectPool.Type.EnemyTank) ObjectPool.addObject(type, new EnemyTank(Row, Column, direction));
+            else if (type == ObjectPool.Type.Gold) ObjectPool.addObject(type, new Gold(Row, Column, direction));
+            else if (type == ObjectPool.Type.StoneWall) ObjectPool.addObject(type, new StoneWall(Row, Column, direction));
+        }
+
+        
+    }
+}
--- TankMaze/Object Pool/ObjectPool.cs
+++ TankMaze/Object Pool/ObjectPool.cs
@@ -1,4 +1,5 @@
-﻿using TankMaze.Controllers;
+﻿using System.Collections;
+using TankMaze.Controllers;
 using TankMaze.Models;
 using TankMaze.Views;
 
@@ -6,20 +7,38 @@ namespace TankMaze.Object_Pool
 {
     static class ObjectPool
     {
+
         private static PlayGround ground;
         private static PlayerTank playerTank;
         private static PlayerTankController playerController;
+        private static EnemyBase enemyBase;
+        private static ArrayList ammos;
+        private static ArrayList bagWalls;
+        private static ArrayList bombs;
+        private static ArrayList bullets;
+        private static ArrayList enemyTanks;
+        private static ArrayList gold;
+        private static ArrayList stoneWalls;
 
         public enum Type
         {
             PlayGround,
-            PlayerTank,PlayerTankController,
+            PlayerTank, PlayerTankController,
+            Ammo, BagsWall, Bomb, Bullet, EnemyBase, EnemyTank, Gold, StoneWall
         }
         public static object getObject(Type type,int index)
         {
             if (type == Type.PlayGround) return ground;
             else if (type == Type.PlayerTank) return playerTank;
             else if (type == Type.PlayerTankController) return playerController;
+            else if (type == Type.EnemyBase) return enemyBase;
+            else if (type == Type.Ammo) return ammos[index];
+            else if (type == Type.BagsWall) return bagWalls[index];
+            else if (type == Type.Bomb) return bombs[index];
+            else if (type == Type.Bullet) return bullets[index];
+            else if (type == Type.EnemyTank) return enemyTanks[index];
+            else if (type == Type.Gold) return gold[index];
+            else if (type == Type.StoneWall) return stoneWalls[index];
             return null;
         }
 
@@ -28,6 +47,14 @@ namespace TankMaze.Object_Pool
             if (type == Type.PlayGround) ground = (PlayGround)Obj;
             else if (type == Type.PlayerTank) playerTank = (PlayerTank)Obj;
             else if (type == Type.PlayerTankController) playerController = (PlayerTankController)Obj;
+            else if (type == Type.EnemyBase) enemyBase = (EnemyBase)Obj;
+            else if (type == Type.Ammo) ammos.Add(Obj);
+            else if (type == Type.BagsWall) bagWalls.Add(Obj);
+            else if (type == Type.Bomb) bombs.Add(Obj);
+            else if (type == Type.Bullet) bullets.Add(Obj);
+            else if (type == Type.EnemyTank) enemyTanks.Add(Obj);
+            else if (type == Type.Gold) gold.Add(Obj);
+            else if (type == Type.StoneWall) stoneWalls.Add(Obj);
         }
     }
 }
