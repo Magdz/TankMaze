e6c1ca767659c5d6bcebe0156ce7ff4b83841377
Basma Adrii <basma.adrii@gmail.com>
2016-01-15 16:33:28 +0200

    Edit MazeFactory

--- TankMaze/Factory/MazeFactory.cs
+++ TankMaze/Factory/MazeFactory.cs
@@ -18,8 +18,9 @@ namespace TankMaze.Factory
             else if (type == ObjectPool.Type.EnemyTank) ObjectPool.addObject(type, new EnemyTank(Row, Column, direction));
             else if (type == ObjectPool.Type.Gold) ObjectPool.addObject(type, new Gold(Row, Column, direction));
             else if (type == ObjectPool.Type.StoneWall) ObjectPool.addObject(type, new StoneWall(Row, Column, direction));
+            else if (type == ObjectPool.Type.PlayerTank) ObjectPool.addObject(type, new PlayerTank(Row, Column, (SingeltonComponent.Direction) direction));
+            else if (type == ObjectPool.Type.EnemyBase) ObjectPool.addObject(type, new EnemyBase(Row, Column, (SingeltonComponent.Direction)direction));
+            else if (type == ObjectPool.Type.Bullet) ObjectPool.addObject(type, new Bullet(Row, Column, (SingeltonComponent.Direction)direction));
         }
-
-        
     }
 }
