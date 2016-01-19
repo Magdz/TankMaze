bc0d527040595eb4d55321dee40b717717ddaf30
Nader Alsharkawy <subnader@gmail.com>
2016-01-19 15:39:22 +0200

    Create state design pattern

---
+++ TankMaze/State/Existent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TankMaze.Object_Pool;
+
+namespace TankMaze.State
+{
+    class Existent : State
+    {
+        //Defines whether an object is destroyable or not
+        bool destroyableState;
+        public Existent(ObjectPool.Type type)
+        {
+            //Handling StoneWall destroyable state on instantiation
+            if (type == ObjectPool.Type.StoneWall) {
+                destroyableState = false;
+            }
+            else {
+                destroyableState = true;
+            }
+        }
+        public bool getState()
+        {
+            return destroyableState;
+        }
+    }
+}
---
+++ TankMaze/State/Nonexistent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TankMaze.Object_Pool;
+
+namespace TankMaze.State
+{
+    class Nonexistent : State
+    {
+        //Defines whether an object is destroyable or not
+        bool destroyableState;
+        public Nonexistent()
+        {
+            //Sets the state to false and self destructs itself
+            destroyableState = false;
+            selfDestruct();
+        }
+        public bool getState()
+        {
+            return destroyableState;
+        }
+
+        private void selfDestruct()
+        {
+            //Self destruct on being set as nonexistent
+        }
+    }
+}
