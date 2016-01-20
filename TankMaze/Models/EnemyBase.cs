051e54b83865f1caa6590c333642bf49fb08b224
Nader Alsharkawy <subnader@gmail.com>
2016-01-20 04:42:30 +0200

    Update Enemy Base

--- TankMaze/Models/EnemyBase.cs
+++ TankMaze/Models/EnemyBase.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows.Controls;
 using System.Windows.Media.Imaging;
 
 namespace TankMaze.Models
@@ -6,24 +7,20 @@ namespace TankMaze.Models
     class EnemyBase : SingeltonComponent
     {
         //int ID = 2;
-        private BitmapImage BaseUp = new BitmapImage(new Uri("pack://application:,,,/TankMaze;component/Assets/BaseUp.png", UriKind.Absolute));
-        private BitmapImage BaseDown = new BitmapImage(new Uri("pack://application:,,,/TankMaze;component/Assets/BaseDown.png", UriKind.Absolute));
-        private BitmapImage BaseLeft = new BitmapImage(new Uri("pack://application:,,,/TankMaze;component/Assets/BaseLeft.png", UriKind.Absolute));
-        private BitmapImage BaseRight = new BitmapImage(new Uri("pack://application:,,,/TankMaze;component/Assets/BaseRight.png", UriKind.Absolute));
-
+        private BitmapImage Base = new BitmapImage(new Uri("pack://application:,,,/TankMaze;component/Assets/EnemyBase.png", UriKind.Absolute));
+        
         public EnemyBase(int Row, int Column, Direction direction) : base(Row, Column)
         {
             if (theEnemyBase != null) return;
             Source(direction);
             theEnemyBase = Instance;
+            theComponent.SetValue(Grid.ColumnSpanProperty, 5);
+            theComponent.SetValue(Grid.RowSpanProperty, 5);
         }
 
         public new void Source(Direction direction)
         {
-            if (direction == Direction.Up) theComponent.Source = BaseUp;
-            else if (direction == Direction.Down) theComponent.Source = BaseDown;
-            else if (direction == Direction.Left) theComponent.Source = BaseLeft;
-            else if (direction == Direction.Right) theComponent.Source = BaseRight;
+            theComponent.Source = Base;
         }
 
     }
